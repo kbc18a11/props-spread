47f3e9c33eeff94206068d8af00c9332a7cb5e0a
kbc18a11 <wada121400@gmail.com>
2023-03-06 23:21:31 +0900

    なるへそ

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+type Props = { message: string, no: number };
 
-function App() {
+const ChildrenText = ({ message, no }: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Message:{message}</div>
+      <div>No:{no}</div>
+    </>
+  );
+}
+
+const App = () => {
+  const displays = {
+    message: 'あああああ',
+    no: 114514
+  };
+
+  return (
+    <div className="App" >
+      <ChildrenText {...displays} />
+    </div >
   );
 }
 
